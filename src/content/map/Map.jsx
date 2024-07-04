d9bc130c7a1dd1f6098ca3cdaecaa528e830675f
Wanderlust-29 <138973321+Wanderlust-29@users.noreply.github.com>
2024-07-04 22:57:45 +0200

    perks and map

--- src/content/map/Map.jsx
+++ src/content/map/Map.jsx
@@ -1,7 +1,18 @@
+import "./Map.css"
+import map from '../../assets/image0.png';
+import location from '../../assets/location.svg';
+
 function Map() {
 
     return (
       <>
+      <div className='map'>
+        <img src={map} alt="Finistère" /> 
+        <div className='landmark'>
+          <img src={location} alt="Localisation" /> 
+        </div>
+      </div>
+      
   
       </>
     )
--- src/content/stat/StatPerks.jsx
+++ src/content/stat/StatPerks.jsx
@@ -1,9 +1,46 @@
 import "./StatPerks.css"
-
+import { useState } from 'react';
 function StatPerks() {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleMouseEnter = (index) => {
+    setActiveIndex(index);
+  };
+
+  const attributes = [
+    { name: 'Logique'},
+    { name: 'Rigoureux'},
+    { name: 'Curieux'},
+  ];
+
   return (
     <div className='perks'>
-
+      <div className="special-left-column">
+        {attributes.map((attribute, index) => (
+          <div
+            key={index}
+            className={`attribute ${activeIndex === index ? 'box-active' : ''}`}
+            onMouseEnter={() => handleMouseEnter(index)}
+          >
+            <p>{attribute.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className='special-right-column'>
+        {/* {images.map((image, index) => (
+          <div key={index} className={`special-attribute-container ${activeIndex === index ? 'active' : ''}`}>
+            <img
+              className={`special-attribute-image ${activeIndex === index ? 'active' : ''}`}
+              src={image}
+              alt={`a fallout vault boy gif representing the ${attributes[index].name} special attribute`}
+            />
+            <p className={`special-attribute-description ${activeIndex === index ? 'active' : ''}`}>
+              {descriptions[index]}
+            </p>
+          </div>
+        ))} */}
+      </div>
     </div>
   );
 }
